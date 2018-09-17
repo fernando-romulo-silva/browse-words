21655e00e46b0fe842f42f8e81ebde9906583f1f
Fernando Romulo da Silva <Fernando.Romulo@usr.brflt.corp>
2018-09-17 17:44:50 -0300

    news

--- src/main/java/br/com/fernando/browsewords/BrowseWordsHtmlUnit.java
+++ src/main/java/br/com/fernando/browsewords/BrowseWordsHtmlUnit.java
@@ -1,7 +1,5 @@
 package br.com.fernando.browsewords;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +7,8 @@ import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.HttpMethod;
@@ -24,98 +22,93 @@ import com.google.common.collect.ListMultimap;
 import com.jayway.jsonpath.JsonPath;
 
 public class BrowseWordsHtmlUnit {
-    
-    private static void lookingForRepeatedWords() throws MalformedURLException, IOException {
-	final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_60);
-	webClient.getOptions().setJavaScriptEnabled(false);
-	webClient.getOptions().setCssEnabled(false);
-
-	// final Multimap<String, String> map = ArrayListMultimap.create();
-	final ListMultimap<String, String> map = ArrayListMultimap.create();
-
-	try (webClient) {
-
-	    final WebRequest webRequest = new WebRequest(new URL("https://quizlet.com/webapi/3.2/feed/65138028/created-sets?perPage=100&query=&sort=alphabetical&seenCreatedSetIds=&filters%5Bsets%5D%5BisPublished%5D=true&include%5Bset%5D%5B%5D=creator"), HttpMethod.GET);
-	    webRequest.setAdditionalHeader("Accept", "*/*");
-	    webRequest.setAdditionalHeader("Content-Type", "application/json");
-
-	    final String jsonString = webClient.<UnexpectedPage>getPage(webRequest) //
-	        .getWebResponse() //
-	        .getContentAsString();
-
-	    /**
-	     * <pre>
-	     *    {"responses":[
-	     *                 	    {
-	     *                        "models": {
-	     *                                    "classSet":[], 
-	     *                 	                  "session":[], 
-	     *                 	    	          "set":[ 
-	     *                                             {	
-	     *                                               "id":306948128, 
-	     *                 	    	    	             "timestamp":1533897810, 
-	     *                 	    	    	             "_webUrl":"https://quizlet.com/306948128/english-expressions-0001-flash-cards/", 
-	     *                 	    	    	              "_thumbnailUrl":null, 
-	     *                 	    	    	              "price":null 
-	     *                                             },
-	     *                                             {   
-	     *                                               "id": 279096228,   
-	     *                                               "timestamp": 1521453734,   
-	     *                                               "_webUrl": "https://quizlet.com/279096228/english-multi-word-verbs-0001-flash-cards/",   
-	     *                                               "_thumbnailUrl": null,   
-	     *                                               "price": null   
-	     *                                             }   
-	     *                                           ]    
-	     *                 	    	        } 
-	     *                      }
-	     *                 ] 
-	     *    }
-	     * </pre>
-	     */
-	    final List<String> urlStudySets = JsonPath.read(jsonString, "$..set[*]._webUrl");
-
-	    for (final String urlStudySet : urlStudySets) {
-
-		final List<HtmlSpan> spans = webClient.<HtmlPage>getPage(urlStudySet) //
-		    .getByXPath("//span[contains(@class,'lang-en')]");
-
-		final List<String> wordsPage = spans.stream() //
-		    .map(elt -> elt.getTextContent()) //
-		    .collect(Collectors.toList());
-
-		wordsPage.forEach(w -> map.put(w, urlStudySet));
-
-		// map.putAll(spans.stream() //
-		// .collect(Collectors //
-		// .toMap(HtmlSpan::getTextContent, (x) -> urlStudySet)));
-	    }
-	}
-
-	System.out.println("Looking for repeated words ");
-
-	for (final Map.Entry<String, Collection<String>> entry : map.asMap().entrySet()) {
-	    if (entry.getValue().size() > 1) {
-		System.out.println(entry.getKey() + " - " + entry.getValue());
-	    }
-	}
-    }
-    
 
     public static void main(String[] args) throws Exception {
 
-	// lookingForRepeatedWords();
+        final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_60);
+        webClient.getOptions().setJavaScriptEnabled(false);
+        webClient.getOptions().setCssEnabled(false);
 
-	System.out.println("Looking for repeated words ");
+        // final Multimap<String, String> map = ArrayListMultimap.create();
+        final ListMultimap<String, String> map = ArrayListMultimap.create();
 
-	Path path = Paths.get(BrowseWordsHtmlUnit.class.getClassLoader().getResource("words.txt").toURI());
+        try (webClient) {
 
-	StringBuilder data = new StringBuilder();
-	Stream<String> lines = Files.lines(path);
-	
-	try (lines){
-	    lines.forEach(line -> data.append(line).append("\n"));
-	    
-	}
+            final WebRequest webRequest = new WebRequest(new URL("https://quizlet.com/webapi/3.2/feed/65138028/created-sets?perPage=100&query=&sort=alphabetical&seenCreatedSetIds=&filters%5Bsets%5D%5BisPublished%5D=true&include%5Bset%5D%5B%5D=creator"), HttpMethod.GET);
+            webRequest.setAdditionalHeader("Accept", "*/*");
+            webRequest.setAdditionalHeader("Content-Type", "application/json");
 
+            final String jsonString = webClient.<UnexpectedPage>getPage(webRequest) //
+                .getWebResponse() //
+                .getContentAsString();
+
+            /**
+             * <pre>
+             *    {"responses":[
+             *                      {
+             *                        "models": {
+             *                                    "classSet":[], 
+             *                                    "session":[], 
+             *                                    "set":[ 
+             *                                             {    
+             *                                               "id":306948128, 
+             *                                               "timestamp":1533897810, 
+             *                                               "_webUrl":"https://quizlet.com/306948128/english-expressions-0001-flash-cards/", 
+             *                                                "_thumbnailUrl":null, 
+             *                                                "price":null 
+             *                                             },
+             *                                             {   
+             *                                               "id": 279096228,   
+             *                                               "timestamp": 1521453734,   
+             *                                               "_webUrl": "https://quizlet.com/279096228/english-multi-word-verbs-0001-flash-cards/",   
+             *                                               "_thumbnailUrl": null,   
+             *                                               "price": null   
+             *                                             }   
+             *                                           ]    
+             *                                  } 
+             *                      }
+             *                 ] 
+             *    }
+             * </pre>
+             */
+            final List<String> urlStudySets = JsonPath.read(jsonString, "$..set[*]._webUrl");
+
+            for (final String urlStudySet : urlStudySets) {
+
+                final List<HtmlSpan> spans = webClient.<HtmlPage>getPage(urlStudySet) //
+                    .getByXPath("//span[contains(@class,'lang-en')]");
+
+                final List<String> wordsPage = spans.stream() //
+                    .map(elt -> elt.getTextContent()) //
+                    .collect(Collectors.toList());
+
+                wordsPage.forEach(w -> map.put(w, urlStudySet));
+
+                // map.putAll(spans.stream() //
+                // .collect(Collectors //
+                // .toMap(HtmlSpan::getTextContent, (x) -> urlStudySet)));
+            }
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------------");
+        System.out.println("Looking for repeated words");
+
+        for (final Map.Entry<String, Collection<String>> entry : map.asMap().entrySet()) {
+            if (entry.getValue().size() > 1) {
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
+        }
+
+        System.out.println("-------------------------------------------------------------------------------------------");
+        System.out.println("Check if words on 'words.txt' are on the site");
+
+        final Path words = Paths.get(BrowseWordsHtmlUnit.class.getClassLoader().getResource("words.txt").toURI());
+
+        final Set<String> wordsOnSite = map.keySet();
+
+        Files.lines(words) // reading file
+            .filter(w -> !wordsOnSite.contains(w.toLowerCase())) // only words that not in site
+            .collect(Collectors.toSet()) // remove duplicates, but disorganize
+            .forEach(System.out::println);
     }
 }
