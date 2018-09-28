b4815def3e874cb9f44d31d632f6a707a1274db9
Fernando Romulo da Silva <Fernando.Romulo@usr.brflt.corp>
2018-09-28 14:50:07 -0300

    news

--- src/main/java/br/com/fernando/browsewords/BrowseWordsJSoups.java
+++ src/main/java/br/com/fernando/browsewords/BrowseWordsJSoups.java
@@ -7,14 +7,14 @@ import org.jsoup.Jsoup;
 public class BrowseWordsJSoups {
 
     public static void main(String[] args) throws IOException {
-	var doc = Jsoup.connect("https://quizlet.com/webapi/3.2/feed/65138028/created-sets?perPage=100&query=&sort=alphabetical&seenCreatedSetIds=&filters%5Bsets%5D%5BisPublished%5D=true&include%5Bset%5D%5B%5D=creator") //
-	    .header("Accept", "*/*") //
-	    .header("Content-Type", "application/json") //
-	    .get() //
-	    .body() //
-	    .text();
+        String doc = Jsoup.connect("https://quizlet.com/webapi/3.2/feed/65138028/created-sets?perPage=100&query=&sort=alphabetical&seenCreatedSetIds=&filters%5Bsets%5D%5BisPublished%5D=true&include%5Bset%5D%5B%5D=creator") //
+                .header("Accept", "*/*") //
+                .header("Content-Type", "application/json") //
+                .get() //
+                .body() //
+                .text();
 
-	System.out.println("Title : " + doc);
+        System.out.println("Title : " + doc);
     }
 
 }
