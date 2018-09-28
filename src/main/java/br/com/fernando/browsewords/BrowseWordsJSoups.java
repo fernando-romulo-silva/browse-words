99574e1880fac105f58d689709264296af160c39
Fernando Romulo da Silva <Fernando.Romulo@usr.brflt.corp>
2018-09-28 18:25:41 -0300

    news

--- src/main/java/br/com/fernando/browsewords/BrowseWordsJSoups.java
+++ src/main/java/br/com/fernando/browsewords/BrowseWordsJSoups.java
@@ -1,20 +1,36 @@
 package br.com.fernando.browsewords;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import com.jayway.jsonpath.JsonPath;
 
 public class BrowseWordsJSoups {
 
     public static void main(String[] args) throws IOException {
-        String doc = Jsoup.connect("https://quizlet.com/webapi/3.2/feed/65138028/created-sets?perPage=100&query=&sort=alphabetical&seenCreatedSetIds=&filters%5Bsets%5D%5BisPublished%5D=true&include%5Bset%5D%5B%5D=creator") //
-                .header("Accept", "*/*") //
-                .header("Content-Type", "application/json") //
-                .get() //
-                .body() //
-                .text();
+        final String jsonString = Jsoup.connect("https://quizlet.com/webapi/3.2/feed/65138028/created-sets?perPage=100&query=&sort=alphabetical&seenCreatedSetIds=&filters%5Bsets%5D%5BisPublished%5D=true&include%5Bset%5D%5B%5D=creator") //
+                .ignoreContentType(true)//
+                .execute() //
+                .body(); //
+        
+        final List<String> urlStudySets = JsonPath.read(jsonString, "$..set[*]._webUrl");
 
-        System.out.println("Title : " + doc);
+        for (final String urlStudySet : urlStudySets) {
+            
+            Document document = Jsoup.parse(new URL(urlStudySet), 10000);
+            
+            final Elements spans = document.select("//a/@href");
+                    
+                    
+                    //.getByXPath("//span[contains(@class,'lang-en')]");
+        }
+        
+        System.out.println("Title : " + jsonString);
     }
 
 }
