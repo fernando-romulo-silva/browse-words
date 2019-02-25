8acda28707c6a7db3fbe1fa278eec67d3127c585
Fernando Romulo da Silva <Fernando.Romulo@usr.brflt.corp>
2019-02-25 18:53:44 -0300

    news'

---
+++ src/test/java/br/com/fernando/browsewords/BrowseWordsUtilsTest.java
@@ -0,0 +1,11 @@
+package br.com.fernando.browsewords;
+
+import org.testng.annotations.Test;
+
+public class BrowseWordsUtilsTest {
+
+    @Test(groups = "integration")
+    public void test() {
+        System.out.println("test!");
+    }
+}
