73cb3fbd6180e1750f62b10770037eb9867834ae
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2022-10-09 12:10:46 +0100

    news

--- src/main/java/br/com/fernando/browsewords/pmdsite/ToolsConfigs.java
+++ src/main/java/br/com/fernando/browsewords/pmdsite/ToolsConfigs.java
@@ -1,6 +1,7 @@
 package br.com.fernando.browsewords.pmdsite;
 
 import static com.gargoylesoftware.htmlunit.BrowserVersion.FIREFOX_78;
+import static com.gargoylesoftware.htmlunit.BrowserVersion.FIREFOX_ESR;
 import static org.apache.commons.lang3.StringUtils.containsAny;
 import static org.apache.commons.lang3.StringUtils.substringAfter;
 import static org.apache.commons.lang3.StringUtils.substringsBetween;
@@ -13,23 +14,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.time.StopWatch;
-
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSpan;
-import com.gargoylesoftware.htmlunit.html.HtmlUnorderedList;
-import com.google.common.collect.ArrayListMultimap;
 
 public class ToolsConfigs {
 
     public static void checkStylesChecks() throws FailingHttpStatusCodeException, IOException {
 	Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
 
-	final var webClient = new WebClient(FIREFOX_78);
+	final var webClient = new WebClient(FIREFOX_ESR);
 	webClient.getOptions().setJavaScriptEnabled(false);
 	webClient.getOptions().setCssEnabled(false);
 
@@ -76,28 +71,26 @@ public class ToolsConfigs {
 
 	Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
 
-	final var webClient = new WebClient(FIREFOX_78);
+	final var webClient = new WebClient(FIREFOX_ESR);
 	webClient.getOptions().setJavaScriptEnabled(false);
 	webClient.getOptions().setCssEnabled(false);
 
 	final var rulesUrls = List.of( //
-			Map.entry("BEST PRACTICES", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_bestpractices.html"), // best practices
-			Map.entry("CODE STYLE", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_codestyle.html"), // code style
-			Map.entry("DESIGN", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_design.html"), // design
-			Map.entry("DOCUMENTATION", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_documentation.html"), // documentation
-			Map.entry("ERROR PRONE", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_errorprone.html"), // error prone
-			Map.entry("MULTITHREADING", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_multithreading.html"), // multi threading
-			Map.entry("PERFORMANCE", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_performance.html"), // perfomance
-			Map.entry("SECURITY", "https://pmd.github.io/pmd-6.42.0/pmd_rules_java_security.html") // security
+			Map.entry("BEST PRACTICES", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_bestpractices.html"), // best practices
+			Map.entry("CODE STYLE", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_codestyle.html"), // code style
+			Map.entry("DESIGN", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_design.html"), // design
+			Map.entry("DOCUMENTATION", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_documentation.html"), // documentation
+			Map.entry("ERROR PRONE", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_errorprone.html"), // error prone
+			Map.entry("MULTITHREADING", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_multithreading.html"), // multi threading
+			Map.entry("PERFORMANCE", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_performance.html"), // perfomance
+			Map.entry("SECURITY", "https://pmd.sourceforge.io/pmd-6.50.0/pmd_rules_java_security.html") // security
 	);
 
 	final var filters = new String[] { //
 		"This rule is replaced".toLowerCase(), //
 		"This rule is deprecated since".toLowerCase(), //
-		"Deprecated since".toLowerCase(), //
 		"This rule has been deprecated".toLowerCase(), //
-		"this rule is deprecated".toLowerCase(), //
-		"This rule has been renamed".toLowerCase() //
+		"this rule is deprecated".toLowerCase() //
 	};
 
 	try (webClient) {
@@ -126,7 +119,7 @@ public class ToolsConfigs {
     }
 
     public static void main(String[] args) throws Exception {
-	pmdRules();
+	checkStylesChecks();
     }
 
 }
